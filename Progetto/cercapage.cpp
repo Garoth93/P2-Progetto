461d3734c99008d472ca148e04d3834572745e0d
Alessio <alessiobrunetta@gmail.com>
2019-06-20 16:00:34 +0200

    avanzamento

--- Progetto/cercapage.cpp
+++ Progetto/cercapage.cpp
@@ -2,23 +2,41 @@
 
 cercapage::cercapage(QWidget* parent):
     QWidget(parent),
-    nomeUno(new QLabel("Nome",this)),
-    casaProdUno(new QLabel("Casa produttrice",this)),
-    nomeDue(new QLineEdit(this)),
-    casaProdDue(new QLineEdit(this)),
-    cercabutton(new QPushButton("Cerca",this)),
-    contenitoreradio(new QGroupBox(tr("Scegli l'oggetto"),this)),
-    rVideoV(new QRadioButton(tr("Videogioco virtuale"),this)),
+    nomeUno(new QLabel("Nome",this)),//creazione label per Nome
+    casaProdUno(new QLabel("Casa produttrice",this)),//creazione label per casa produttrice
+    nomeDue(new QLineEdit(this)),//creazione edit line per inserimento nome
+    casaProdDue(new QLineEdit(this)),//creazione edit line per inserimento casa produttrice
+    cercabutton(new QPushButton("Cerca",this)),//creazione bottone cerca
+    contenitoreradio(new QGroupBox(tr("Scegli l'oggetto"),this)),//creazione contenitore groupbox
+    rVideoV(new QRadioButton(tr("Videogioco virtuale"),this)),//creazione 3 radio button per scegliere il tipo dell oggetto
     rVideoF(new QRadioButton(tr("Videogioco fisico"),this)),
-    rCarte(new QRadioButton(tr("Gioco di carte"),this))
-
+    rCarte(new QRadioButton(tr("Gioco di carte"),this)),
+    wvirtualgame(new QWidget(this)),//crazione widget per i dati del gioco virtuale
+    wphysicalgame(new QWidget(this)),//crazione widget per i dati del gioco fisico
+    wcardgame(new QWidget(this)),//crazione widget per i dati del gioco dic arte
+    seasonpassv(new QCheckBox(this)),//ceckbox per bool seasonpass gioco virtuale
+    primaedc(new QCheckBox(this)),
+    starterdeckc(new QCheckBox(this)),
+    lipiattaformav(new QLineEdit(this)),//creazione line per piattaforma gioco virtuale
+    liconsolephy(new QLineEdit(this)),
+    liedizionephy(new QLineEdit(this)),
+    liespansione(new QLineEdit(this)),
+    linumerocarte(new QLineEdit(this))
 {
-    QGridLayout * mainl = new QGridLayout;//layout come matrices
-    mainl->addWidget(nomeUno,0,0,1,1);
-    mainl->addWidget(casaProdUno,1,0,1,1);
-    mainl->addWidget(nomeDue,0,1,1,1);
-    mainl->addWidget(casaProdDue,1,1,1,1);
+    QGridLayout * mainl = new QGridLayout;//layout principale come matrice
 
+    QWidget * witem = new QWidget(this);//widget per inizio ricerca
+
+    //oggetto base item
+    QGridLayout * litem = new QGridLayout;
+    litem->addWidget(nomeUno,0,0,1,1);//aggiunta al mainl
+    litem->addWidget(casaProdUno,1,0,1,1);
+    litem->addWidget(nomeDue,0,1,1,1);
+    litem->addWidget(casaProdDue,1,1,1,1);
+    witem->setLayout(litem);
+    mainl->addWidget(witem,0,0,1,1);
+
+    //radio button
     QVBoxLayout *vbox = new QVBoxLayout;//creo vbox per contenere i radio button
     rVideoV->setChecked(true);//imposto radio button predefinito
     vbox->addWidget(rVideoV);
@@ -27,14 +45,58 @@ cercapage::cercapage(QWidget* parent):
     vbox->addStretch(1);
     contenitoreradio->setLayout(vbox);
     contenitoreradio->setFixedHeight(110);
-    mainl->addWidget(contenitoreradio,2,0,1,1);
+    mainl->addWidget(contenitoreradio,2,0,1,1);//aggiunta radio button a mainl
 
-    mainl->addWidget(cercabutton,3,0,1,1);
+    //bottone cerca
+    mainl->addWidget(cercabutton,4,0,1,1);//aggiunta a mainl bottone cerca
 
-    connect(rVideoV,SIGNAL(clicked()),this,SLOT(radiobuttonslot()));
+    //connect per radio button per interfaccia dinamica
+    connect(rVideoV,SIGNAL(clicked()),this,SLOT(radiobuttonslot()));//crazione signal per vederer cosa visualizzare
     connect(rVideoF,SIGNAL(clicked()),this,SLOT(radiobuttonslot()));
     connect(rCarte,SIGNAL(clicked()),this,SLOT(radiobuttonslot()));
 
+    //ricerca videogioco virtuale
+    QGridLayout * lvirtualgame = new QGridLayout;//layout come matrice per virtual game
+    QLabel * lpiattaformav = new QLabel("Nome",this);//creazione label per piattaforma gioco virtuale
+    QLabel * lseasonpassv = new QLabel("Season pass",this);
+    lvirtualgame->addWidget(lpiattaformav,0,0,1,1);
+    lvirtualgame->addWidget(lipiattaformav,0,1,1,1);
+    lvirtualgame->addWidget(lseasonpassv,1,0,1,1);
+    lvirtualgame->addWidget(seasonpassv,1,1,1,1);
+    wvirtualgame->setLayout(lvirtualgame);
+    mainl->addWidget(wvirtualgame,3,0,1,1);
+
+    //ricerca videogioco fisico
+    QGridLayout * lphygame = new QGridLayout;//layout come matrice per gioco fisico
+    QLabel * lconsolephy = new QLabel("Nome console",this);
+    QLabel * ledizionephy = new QLabel("Edizione",this);
+    lphygame->addWidget(lconsolephy,0,0,1,1);
+    lphygame->addWidget(liconsolephy,0,1,1,1);
+    lphygame->addWidget(ledizionephy,1,0,1,1);
+    lphygame->addWidget(liedizionephy,1,1,1,1);
+    wphysicalgame->setLayout(lphygame);
+    wphysicalgame->setVisible(false);
+    mainl->addWidget(wphysicalgame,3,0,1,1);
+
+    //ricerca gioco di carte
+    QGridLayout * lcardgame = new QGridLayout;//layout come matrice per gioco fisico
+    QLabel * lespansione = new QLabel("Espansione",this);
+    QLabel * lnumerocarte = new QLabel("Numero delle carte",this);
+    QLabel * lprimaedc = new QLabel("Prima edizione",this);
+    QLabel * lstarterdeckc = new QLabel("Season pass",this);
+    lcardgame->addWidget(lespansione,0,0,1,1);
+    lcardgame->addWidget(liespansione,0,1,1,1);
+    lcardgame->addWidget(lnumerocarte,1,0,1,1);
+    lcardgame->addWidget(linumerocarte,1,1,1,1);
+    lcardgame->addWidget(lstarterdeckc,2,0,1,1);
+    lcardgame->addWidget(starterdeckc,2,1,1,1);
+    lcardgame->addWidget(lprimaedc,3,0,1,1);
+    lcardgame->addWidget(primaedc,3,1,1,1);
+    wcardgame->setLayout(lcardgame);
+    wcardgame->setVisible(false);
+    mainl->addWidget(wcardgame,3,0,1,1);
+
+    //aggiornamento layout principale
     setLayout(mainl);
 }
 
@@ -42,6 +104,18 @@ void cercapage::radiobuttonslot() {
         QRadioButton* srb = qobject_cast<QRadioButton*>(sender());
         std::string name = srb->text().toStdString();
         if(name=="Videogioco virtuale"){
+            wvirtualgame->setVisible(true);
+            wphysicalgame->setVisible(false);
+            wcardgame->setVisible(false);
+        }
+        if(name=="Videogioco fisico"){
+            wvirtualgame->setVisible(false);
+            wphysicalgame->setVisible(true);
+            wcardgame->setVisible(false);
+        }
+        if(name=="Gioco di carte"){
+            wvirtualgame->setVisible(false);
+            wphysicalgame->setVisible(false);
+            wcardgame->setVisible(true);
         }
 }
-
--- Progetto/cercapage.h
+++ Progetto/cercapage.h
@@ -12,25 +12,44 @@
 #include <QGridLayout>
 #include <QRadioButton>
 #include <QGroupBox>
+#include <QCheckBox>
 
 class cercapage: public QWidget
 {
     Q_OBJECT
 private:
+    //per item base
     QLabel * nomeUno;
     QLabel * casaProdUno;
     QLineEdit * nomeDue;
     QLineEdit * casaProdDue;
+    //bootone cerca
     QPushButton * cercabutton;//bottone cerca
-
+    //widget per virtuale fisico e carte
+    QWidget * wvirtualgame;//widget per interfaccia virtual game
+    QWidget * wphysicalgame;//widget per interfaccia virtual game
+    QWidget * wcardgame;//widget card game
     //radio button
     QGroupBox *contenitoreradio;//contenitore per i radio button
     QRadioButton *rVideoV;//creo tre puntatori a radio button
     QRadioButton *rVideoF;
     QRadioButton *rCarte;
+    //checkbox
+    QCheckBox * seasonpassv;//checkbox per gioco virtuale
+    QCheckBox * primaedc;
+    QCheckBox * starterdeckc;
+    //gioco virtuale
+    QLineEdit * lipiattaformav;//line edit per inseriemtno piattaforma
+    //gioco fisico
+    QLineEdit * liconsolephy;
+    QLineEdit * liedizionephy;
+    //gioco di carte
+    QLineEdit * liespansione;
+    QLineEdit * linumerocarte;
 public slots://creo slot per signal
     void radiobuttonslot();
 public:
+    //creo un metodo che guarda tutti i checkbox per con is cecked e nel mentre mi cambia le variabili
     cercapage(QWidget* parent);
 };
 
